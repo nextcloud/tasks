3292dfeee9834d5c6c03b5bcb09e56345d59cc95
Raimund Schlüßler <raimund.schluessler@googlemail.com>
2015-07-09 23:57:51 +0200

    Fix update script

--- appinfo/update.php
+++ appinfo/update.php
@@ -35,19 +35,22 @@ if ( version_compare($installedVersionTasksEnhanced, '0.4.1', '<=') && version_c
 
 						$idx = 0;
 						foreach ($children as $i => &$property) {
-							if ( $property->name == 'COMMENT' && $property['ID']->value == (int)$com['ID']->value ) {
-								unset($vtodo->children[$idx]);
+							if ( $property['ID'] && $com['ID']) {
+								if ( $property->name == 'COMMENT' && $property['ID']->getValue() == (int)$com['ID']->getValue() ) {
+									unset($vtodo->children[$idx]);
+								}
 							}
 							$idx += 1;
 						}
-						$vtodo->addProperty('COMMENT',$com->value,
-							array(
-								'X-OC-ID' => (int)$com['ID']->value,
-								'X-OC-USERID' => $com['USERID']->value,
-								'X-OC-DATE-TIME' => $com['DATE-TIME']->value
-								)
-							);
-
+						if ( $com['ID'] && $com['USERID'] && $com['DATE-TIME'] ) {
+							$vtodo->add('COMMENT',$com->getValue(),
+								array(
+									'X-OC-ID' => (int)$com['ID']->getValue(),
+									'X-OC-USERID' => $com['USERID']->getValue(),	
+									'X-OC-DATE-TIME' => $com['DATE-TIME']->getValue()
+									)
+								);
+						}
 						OCP\Util::emitHook('OC_Calendar', 'editEvent', $taskId);
 					}
 					$data = $vcalendar->serialize();
@@ -66,10 +69,10 @@ if ( version_compare($installedVersionTasksEnhanced, '0.4.1', '<=') && version_c
 						if($property->name == 'VTODO') {
 							foreach($property->children as &$element) {
 								if($element->name == 'SUMMARY') {
-									$summary = $element->value;
+									$summary = $element->getValue();
 								}
 								elseif($element->name == 'UID') {
-									$uid = $element->value;
+									$uid = $element->getValue();
 								}
 							};
 							break;
