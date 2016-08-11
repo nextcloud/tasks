d758d2637500405fa29af0cea1feccb648b7848a
Raimund Schlüßler <raimund.schluessler@googlemail.com>
2016-08-11 17:20:00 +0200

    Increase version number

--- js/public/app.js
+++ js/public/app.js
@@ -1,7 +1,7 @@
 (function(angular, $, oc_requesttoken, undefined){
 
 /**
- * ownCloud Task App - v0.9.2
+ * ownCloud Task App - v0.9.3
  *
  * Copyright (c) 2016 - Raimund Schlüßler <raimund.schluessler@googlemail.com>
  *
