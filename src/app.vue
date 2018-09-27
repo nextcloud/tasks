ae1df4c9d9d179fb4b1761b90dc84a36c8b085f0
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2018-09-27 21:45:27 +0200

    Show task input field with correct placeholder

--- src/app.vue
+++ src/app.vue
@@ -26,80 +26,9 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 			<theSettings />
 		</div>
 
-		<div id="app-content"
-			ng-class="{'with-app-sidebar':route.taskID}">
+		<div id="app-content">
 			<div class="content-wrapper">
-				<div id="add-task"
-					class="add-task handler"
-					ng-show="showInput()"
-					ng-class="{'focus':status.focusTaskInput}">
-					<form ng-submit="addTask(status.taskName)" name="addTaskForm">
-						<input id="target"
-							ng-disabled="isAddingTask"
-							ng-click="focusTaskInput()"
-							class="transparent"
-							ng-model="status.taskName"
-							ng-keydown="checkTaskInput($event)">
-							<!-- placeholder="{{ getAddString() }}"> -->
-					</form>
-				</div>
-				<div class="app-navigation-entry-utils">
-					<div class="app-navigation-entry-utils-menu-button" title="<?php p($l->t('Change sort order')); ?>">
-						<button class="sortorder-dropdown-button">
-							<span class="icon" ng-class="getSortOrderIcon()" />
-							<span class="icon sort-indicator" ng-class="{'icon-sort-up': settingsmodel.getById('various').sortDirection, 'icon-sort-down': !settingsmodel.getById('various').sortDirection}" />
-						</button>
-					</div>
-				</div>
-				<div class="app-navigation-entry-menu bubble sortorder-dropdown">
-					<ul>
-						<li ng-click="setSortOrder($event, 'default')" ng-class="{active: settingsmodel.getById('various').sortOrder == 'default'}" class="handler">
-							<a>
-								<span class="icon icon-list" />
-								<span class="label">{{ t('tasks', 'Default') }}</span>
-								<span class="icon sort-indicator" ng-class="{'icon-sort-up': settingsmodel.getById('various').sortDirection, 'icon-sort-down': !settingsmodel.getById('various').sortDirection}" />
-							</a>
-						</li>
-						<li ng-click="setSortOrder($event, 'due')" ng-class="{active: settingsmodel.getById('various').sortOrder == 'due'}">
-							<a>
-								<span class="icon icon-calendar" />
-								<span class="label">{{ t('tasks', 'Due date') }}</span>
-								<span class="icon sort-indicator" ng-class="{'icon-sort-up': settingsmodel.getById('various').sortDirection, 'icon-sort-down': !settingsmodel.getById('various').sortDirection}" />
-							</a>
-						</li>
-						<li ng-click="setSortOrder($event, 'start')" ng-class="{active: settingsmodel.getById('various').sortOrder == 'start'}">
-							<a>
-								<span class="icon icon-calendar" />
-								<span class="label">{{ t('tasks', 'Start date') }}</span>
-								<span class="icon sort-indicator" ng-class="{'icon-sort-up': settingsmodel.getById('various').sortDirection, 'icon-sort-down': !settingsmodel.getById('various').sortDirection}" />
-							</a>
-						</li>
-						<li ng-click="setSortOrder($event, 'priority')" ng-class="{active: settingsmodel.getById('various').sortOrder == 'priority'}">
-							<a>
-								<span class="icon icon-task-star" />
-								<span class="label">{{ t('tasks', 'Priority') }}</span>
-								<span class="icon sort-indicator" ng-class="{'icon-sort-up': settingsmodel.getById('various').sortDirection, 'icon-sort-down': !settingsmodel.getById('various').sortDirection}" />
-							</a>
-						</li>
-						<li ng-click="setSortOrder($event, 'alphabetically')" ng-class="{active: settingsmodel.getById('various').sortOrder == 'alphabetically'}">
-							<a>
-								<span class="icon icon-alphabetically" />
-								<span class="label">{{ t('tasks', 'Alphabetically') }}</span>
-								<span class="icon sort-indicator" ng-class="{'icon-sort-up': settingsmodel.getById('various').sortDirection, 'icon-sort-down': !settingsmodel.getById('various').sortDirection}" />
-							</a>
-						</li>
-						<!-- <li ng-click="setSortOrder($event, 'manual')" ng-class="{active: settingsmodel.getById('various').sortOrder == 'manual'}">
-							<a>
-								<span class="icon icon-manual" />
-								<span class="label">{{ t('tasks', 'Manually') }</span>
-								<span class="icon sort-indicator" ng-class="{'icon-sort-up': settingsmodel.getById('various').sortDirection, 'icon-sort-down': !settingsmodel.getById('various').sortDirection}" />
-							</a>
-						</li> -->
-					</ul>
-				</div>
-				<div class="task-list">
-					<router-view />
-				</div>
+				<router-view />
 			</div>
 		</div>
 
