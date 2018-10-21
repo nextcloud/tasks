f8dcacdeef1f31c0a648f6f3f94bebd9aea194ea
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2018-10-21 21:54:26 +0200

    Add configuration for dynamic module loading

--- src/main.js
+++ src/main.js
@@ -29,6 +29,17 @@ import store from './store/store'
 import { sync } from 'vuex-router-sync'
 import VTooltip from 'v-tooltip'
 
+// CSP config for webpack dynamic chunk loading
+// eslint-disable-next-line
+__webpack_nonce__ = btoa(OC.requestToken)
+
+// Correct the root of the app for chunk loading
+// OC.linkTo matches the apps folders
+// OC.generateUrl ensure the index.php (or not)
+// We do not want the index.php since we're loading files
+// eslint-disable-next-line
+__webpack_public_path__ = OC.linkTo('tasks', 'js/')
+
 sync(store, router)
 
 Vue.use(VTooltip)
