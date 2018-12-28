5012a0138d9d8b5b9956a363d42268b6b1c95f75
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2018-12-28 10:10:39 +0100

    Use diff function for date sorting

--- src/store/storeHelper.js
+++ src/store/storeHelper.js
@@ -179,7 +179,7 @@ function sortByDate(tasks, date) {
 			return 0
 		}
 
-		return moment(taskA[date], 'YYYYMMDDTHHmmss').format('YYYYMMDDHHmm') - moment(taskB[date], 'YYYYMMDDTHHmmss').format('YYYYMMDDHHmm')
+		return moment(taskA[date], 'YYYYMMDDTHHmmss').diff(moment(taskB[date], 'YYYYMMDDTHHmmss'))
 	})
 }
 
