3202dd7823b9ff55f62818527374168fc40c31fd
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2018-12-29 16:43:01 +0100

    Remove useless comments

--- src/components/TheCollections/Calendar.vue
+++ src/components/TheCollections/Calendar.vue
@@ -48,7 +48,6 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 						:key="task.id"
 						:task="task"
 					/>
-					<!-- ng-repeat="task in filtered = filteredTasks() | filter:hasNoParent(task) | filter:filterTasks(task,route.calendarID) | filter:{'completed':'false'} | orderBy:getSortOrder():settingsmodel.getById('various').sortDirection"> -->
 				</ol>
 				<h2 v-show="completedCount(calendarId)" class="heading-hiddentasks icon-triangle-s" @click="toggleHidden">
 					{{ completedCountString }}
@@ -63,7 +62,6 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 						:key="task.id"
 						:task="task"
 					/>
-					<!-- ng-repeat="task in filtered = filteredTasks() | filter:hasNoParent(task) | filter:filterTasks(task,route.calendarID) | filter:{'completed':true} | orderBy:'completed_date':true"> -->
 				</ol>
 				<LoadCompletedButton :calendar="calendar" />
 			</div>
