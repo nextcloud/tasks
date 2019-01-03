053d4d567e4ee906de16d8094c2ed0520f14a80a
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2019-01-03 17:34:04 +0100

    Change load completed tasks string

--- src/components/LoadCompletedButton.vue
+++ src/components/LoadCompletedButton.vue
@@ -22,7 +22,7 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 <template>
 	<div v-show="!loadedCompleted" class="loadmore">
 		<span @click="loadCompletedTasks">
-			{{ t('tasks', 'Load remaining completed tasks.') }}
+			{{ t('tasks', 'Load all completed tasks.') }}
 		</span>
 	</div>
 </template>
