da10bb9a589485ebd9c9276136a019e762995611
Tim Hollmann <github.fe8c53c0@mail.tim-hollmann.de>
2019-10-31 10:14:02 +0000

    Apply suggestions, switched icons
    Co-Authored-By: Raimund Schlüßler <raimund.schluessler@mailbox.org>
    Signed-off-by: Tim Hollmann <github.fe8c53c0@mail.tim-hollmann.de>

--- src/components/TheDetails.vue
+++ src/components/TheDetails.vue
@@ -40,13 +40,13 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 					>
 					<label :for="'detailsToggleCompleted_' + task.uid" />
 				</span>
-				<a class="star reactive" @click="togglePinned(task)">
-					<span :class="[{'disabled': task.calendar.readOnly}, iconPinned]" class="icon" />
-				</a>
 				<a class="star reactive" @click="toggleStarred(task)">
 					<span :class="[{'disabled': task.calendar.readOnly}, iconStar]"
 						class="icon"
 					/>
+				</a>
+				<a class="star reactive" @click="togglePinned(task)">
+					<span :class="[{'disabled': task.calendar.readOnly}, iconPinned]" class="icon" />
 				</a>
 				<TaskStatusDisplay :task="task" />
 				<div v-click-outside="() => finishEditing('summary')">
@@ -591,9 +591,9 @@ export default {
 		},
 		iconPinned: function() {
 			if (this.task.pinned) {
-				return 'icon-bw icon-pinned-off'
-			} else {
 				return 'icon-bw icon-pinned'
+			} else {
+				return 'icon-bw icon-pinned-off'
 			}
 		},
 		iconPercent: function() {
