1ac8a278e33c4eeca1be489331d206effb6a8ed3
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2019-11-02 20:01:53 +0100

    Add test for current view Signed-off-by: Raimund Schlüßler <raimund.schluessler@mailbox.org>

--- tests/javascript/unit/components/TheCollections/General.spec.js
+++ tests/javascript/unit/components/TheCollections/General.spec.js
@@ -10,6 +10,9 @@ localVue.use(VTooltip)
 describe('General.vue', () => {
 	'use strict'
 
+	/*
+	 * The all view shows uncompleted tasks
+	 */
 	it('Checks that we get the correct number of calendars for the all view', () => {
 		const wrapper = mount(General, { localVue, store, router })
 		if (wrapper.vm.$route.params.collectionId !== 'all') {
@@ -27,6 +30,9 @@ describe('General.vue', () => {
 		expect(wrapper.find('li[task-id="pwen4kz19g.ics"]').exists()).toBe(false)
 	})
 
+	/*
+	 * The starred view shows important and uncompleted tasks
+	 */
 	it('Checks that we get the correct number of calendars for the starred view', () => {
 		const wrapper = mount(General, { localVue, store, router })
 		if (wrapper.vm.$route.params.collectionId !== 'starred') {
@@ -47,6 +53,9 @@ describe('General.vue', () => {
 		expect(wrapper.find('li[task-id="pwen4kz25g.ics"]').exists()).toBe(false)	// Has an important sibling, but no important child --> hidden
 	})
 
+	/*
+	 * The current view shows uncompleted tasks which don't have a start date in the future
+	 */
 	it('Checks that we get the correct number of calendars for the current view', () => {
 		const wrapper = mount(General, { localVue, store, router })
 		if (wrapper.vm.$route.params.collectionId !== 'current') {
@@ -55,6 +64,21 @@ describe('General.vue', () => {
 		expect(wrapper.vm.filteredCalendars.length).toBe(2)
 	})
 
+	it('Checks that only current tasks show in the current view', () => {
+		const wrapper = mount(General, { localVue, store, router })
+		if (wrapper.vm.$route.params.collectionId !== 'current') {
+			router.push({ name: 'collections', params: { collectionId: 'current' } })
+		}
+		expect(wrapper.find('li[task-id="pwen4kz18g.ics"]').exists()).toBe(true)	// No start or due date			--> shown
+		expect(wrapper.find('li[task-id="pwen4kz19g.ics"]').exists()).toBe(false)	// Start date in the future		--> hidden
+		expect(wrapper.find('li[task-id="pwen4kz20g.ics"]').exists()).toBe(true)	// Start date in the future, but current subsubtask	--> shown
+		expect(wrapper.find('li[task-id="pwen4kz24g.ics"]').exists()).toBe(true)	// Current subsubtask 			--> shown
+		expect(wrapper.find('li[task-id="pwen4kz25g.ics"]').exists()).toBe(false)	// Start date in the future and no current child	--> hidden
+	})
+
+	/*
+	 * The today view shows uncompleted tasks which started or are due today or earlier
+	 */
 	it('Checks that we get the correct number of calendars for the today view', () => {
 		const wrapper = mount(General, { localVue, store, router })
 		if (wrapper.vm.$route.params.collectionId !== 'today') {
@@ -63,6 +87,9 @@ describe('General.vue', () => {
 		expect(wrapper.vm.filteredCalendars.length).toBe(0)
 	})
 
+	/*
+	 * The completed view shows completed tasks
+	 */
 	it('Checks that we get the correct number of calendars for the completed view', () => {
 		const wrapper = mount(General, { localVue, store, router })
 		if (wrapper.vm.$route.params.collectionId !== 'completed') {
