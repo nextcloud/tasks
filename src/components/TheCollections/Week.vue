2f693c44492b24f43d64398d4b1f172882469dd4
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2019-12-04 20:38:00 +0100

    Add task due in one week in week view
    Signed-off-by: Raimund Schlüßler <raimund.schluessler@mailbox.org>

--- src/components/TheCollections/Week.vue
+++ src/components/TheCollections/Week.vue
@@ -22,6 +22,19 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 <template>
 	<div>
+		<div v-if="calendar && !calendar.readOnly"
+			id="add-task"
+			class="add-task"
+		>
+			<form name="addTaskForm" @submit.prevent="addTask">
+				<input v-model="newTaskName"
+					:placeholder="inputString"
+					:disabled="isAddingTask"
+					class="transparent reactive"
+					@keyup.27="clearNewTask($event)"
+				>
+			</form>
+		</div>
 		<SortorderDropdown />
 		<div class="task-list">
 			<div v-for="day in days" :key="day.diff" :day="day.diff"
@@ -47,7 +60,7 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import { sort, isTaskInList } from '../../store/storeHelper'
 import SortorderDropdown from '../SortorderDropdown'
 import Task from '../Task'
@@ -59,8 +72,15 @@ export default {
 		SortorderDropdown,
 		TaskDragContainer,
 	},
+	data() {
+		return {
+			newTaskName: '',
+			isAddingTask: false
+		}
+	},
 	computed: {
 		...mapGetters({
+			calendar: 'getDefaultCalendar',
 			tasks: 'getAllTasks',
 			uncompletedRootTasks: 'findUncompletedRootTasks',
 			sortOrder: 'sortOrder',
@@ -91,9 +111,17 @@ export default {
 
 			// Remove all days without tasks.
 			return days.filter(day => day.tasks.length)
-		}
+		},
+
+		inputString: function() {
+			return this.$t('tasks', 'Add a task due in one week to "{calendar}"…', { calendar: this.calendar.displayName })
+		},
 	},
 	methods: {
+		...mapActions([
+			'createTask'
+		]),
+
 		sort,
 
 		dayString: function(day) {
@@ -108,6 +136,21 @@ export default {
 			}
 			return dayString + ', ' + date.format('LL')
 		},
+
+		clearNewTask: function(event) {
+			event.target.blur()
+			this.newTaskName = ''
+		},
+
+		addTask: function() {
+			var task = { summary: this.newTaskName }
+
+			task.due = moment().add(7, 'days').startOf('day').format('YYYY-MM-DDTHH:mm:ss')
+			task.allDay = this.$store.state.settings.settings.allDay
+
+			this.createTask(task)
+			this.newTaskName = ''
+		}
 	}
 }
 </script>
