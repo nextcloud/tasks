b9811246bf9c5808ba24ad1c78f8592eb7ff07d7
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2020-03-08 10:57:20 +0100

    Better handle calendars without color
    Signed-off-by: Raimund Schlüßler <raimund.schluessler@mailbox.org>

---
+++ src/utils/color.js
@@ -0,0 +1,55 @@
+/**
+ * @copyright Copyright (c) 2019 Georg Ehrke
+ *
+ * @author Georg Ehrke <oc.list@georgehrke.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+import convert from 'color-convert'
+import { uidToColor } from './uidToColor.js'
+
+/**
+ * Generates a hex color based on RGB string
+ *
+ * @param {String} uid The string to generate a color from
+ * @returns {string} The hex color
+ */
+export function uidToHexColor(uid) {
+	const color = uidToColor(uid)
+	return '#' + convert.rgb.hex(color.r, color.g, color.b)
+}
+
+/**
+ * Detects a color from a given string
+ *
+ * @param {String} color The color to get the real RGB hex string from
+ * @returns {string|boolean|*} String if color detected, boolean if not
+ */
+export function detectColor(color) {
+	if (/^(#)((?:[A-Fa-f0-9]{3}){1,2})$/.test(color)) { // #ff00ff and #f0f
+		return color
+	} else if (/^((?:[A-Fa-f0-9]{3}){1,2})$/.test(color)) { // ff00ff and f0f
+		return '#' + color
+	} else if (/^(#)((?:[A-Fa-f0-9]{8}))$/.test(color)) { // #ff00ffff and #f0ff
+		return color.substr(0, 7)
+	} else if (/^((?:[A-Fa-f0-9]{8}))$/.test(color)) { // ff00ffff and f0ff
+		return '#' + color.substr(0, 6)
+	}
+
+	return false
+
+}
