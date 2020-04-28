335c7f630ac911af96e8c7cd93322f7c6f55f537
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2020-04-28 22:20:26 +0200

    Remember last route
    Signed-off-by: Raimund Schlüßler <raimund.schluessler@mailbox.org>

---
+++ src/utils/router.js
@@ -0,0 +1,39 @@
+/**
+ * Nextcloud - Tasks
+ *
+ * @author Georg Ehrke <oc.list@georgehrke.com>
+ * @copyright Copyright (c) 2020 Georg Ehrke
+ *
+ * @author Raimund Schlüßler
+ * @copyright 2020 Raimund Schlüßler <raimund.schluessler@mailbox.org>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+import { loadState } from '@nextcloud/initial-state'
+
+/**
+ * Gets the initial route
+ *
+ * @returns {String}
+ */
+export function getInitialRoute() {
+	try {
+		return loadState('tasks', 'initialRoute')
+	} catch (error) {
+		return '/collections/all'
+	}
+}
