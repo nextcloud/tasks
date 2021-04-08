59bb1f21a464862adebbe6f1484e95242ab7901c
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2021-04-08 10:02:05 +0200

    Correct import statement
    Signed-off-by: Raimund Schlüßler <raimund.schluessler@mailbox.org>

--- src/views/AppNavigation.vue
+++ src/views/AppNavigation.vue
@@ -97,8 +97,8 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 <script>
 import ListItemCalendar from '../components/AppNavigation/ListItemCalendar.vue'
-import Colorpicker from '../components/AppNavigation//Colorpicker.vue'
-import TheSettings from '../components/AppNavigation//TheSettings.vue'
+import Colorpicker from '../components/AppNavigation/Colorpicker.vue'
+import TheSettings from '../components/AppNavigation/TheSettings.vue'
 
 import AppNavigation from '@nextcloud/vue/dist/Components/AppNavigation'
 import AppNavigationCounter from '@nextcloud/vue/dist/Components/AppNavigationCounter'
