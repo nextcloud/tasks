8d3d1d4db45f6bb173848e9cd09be44d3ea85edf
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2021-08-28 21:52:21 +0200

    Use @nextcloud/calendar-js
    Signed-off-by: Raimund Schlüßler <raimund.schluessler@mailbox.org>

--- src/models/calendarObject.js
+++ src/models/calendarObject.js
@@ -21,7 +21,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-import { getParserManager } from 'calendar-js'
+import { getParserManager } from '@nextcloud/calendar-js'
 import {
 	COMPONENT_NAME_EVENT,
 	COMPONENT_NAME_JOURNAL,
