d9da7becdf59d9f20290d50aee835e4405867069
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2022-04-23 20:55:22 +0200

    Cleanup test configuration
    Signed-off-by: Raimund Schlüßler <raimund.schluessler@mailbox.org>

--- tests/javascript/unit/setup.js
+++ tests/javascript/unit/setup.js
@@ -1,6 +1,5 @@
 import { OC } from './OC.js'
 
-import { config } from '@vue/test-utils'
 // eslint-disable-next-line node/no-unpublished-import
 import MockDate from 'mockdate'
 // eslint-disable-next-line node/no-unpublished-import
@@ -12,22 +11,16 @@ MockDate.set(new Date('2019-01-01T12:34:56'))
 global.OC = new OC()
 
 // Mock nextcloud translate functions
-config.mocks.$t = function(app, string) {
+global.t = function(app, string) {
 	return string
 }
-config.mocks.t = config.mocks.$t
-global.t = config.mocks.$t
 
-config.mocks.$n = function(app, singular, plural, count) {
+global.n = function(app, singular, plural, count) {
 	return singular
 }
-config.mocks.n = config.mocks.$n
-global.n = config.mocks.$n
 
 global.OCA = {}
 global.OCA.Tasks = {}
-global.OCA.Tasks.$t = config.mocks.$t
-global.OCA.Tasks.$n = config.mocks.$n
 
 afterAll(() => {
 	MockDate.reset()
