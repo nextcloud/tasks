6552e2abcb9cfb8825c0bde3b01e14981ddb03d0
Stefan Forstenlechner <t-h-e@users.noreply.github.com>
2023-04-20 22:25:47 +0200

    add text to task converter
    Signed-off-by: Stefan Forstenlechner <t-h-e@users.noreply.github.com>

---
+++ src/utils/textToTask.js
@@ -0,0 +1,91 @@
+/**
+ * Nextcloud - Tasks
+ *
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+/*
+task
+- task
++ task
+* task
+- [] task
+- [x] task
+- [X] task
+- [ ] task
+*/
+
+/*
+- task
+	- task
+	- task
+		- task
+	- task
+*/
+
+export const textToTask = (text) => {
+	var lines = text.split(/\r?\n/)
+		.filter(isNotOnlyWhitespace)
+		.map(spaceToTab)
+
+	const rootTask = {
+		title: "ROOT",
+		parent: undefined,
+		depth: 0,
+		children: []
+	}
+	var curTask = rootTask
+
+	while(lines.length){
+		var line = lines.shift()
+		var curDepth = countSpacesDepth(line)
+
+		while (curTask.parent !== undefined && curDepth <= curTask.depth) {
+			curTask = curTask.parent
+		}
+
+		var prefix = listLikePrefix(line)
+		var title = line.substring(prefix.length)
+		var nextTask = {
+			title: title,
+			parent: curTask,
+			depth: curDepth,
+			children: []
+		}
+		curTask.children.push(nextTask)
+		curTask = nextTask
+	}
+
+	return cleanTasks(rootTask.children)
+}
+
+const cleanTasks = (tasks) => tasks.map(t => ({
+	title: t.title,
+	children: cleanTasks(t.children)
+}))
+
+const isNotOnlyWhitespace = (s) => {
+	return /\S/.test(s)
+}
+
+// tab counts as 4 spaces
+const spaceToTab = (s) => s.replace(/\t/g, '    ')
+
+const listLikePrefix = (s) => s.match(/^([-+*\s]*(\[(\s|x|X)?]\s*)?)/)?.[0] || ""
+
+const countSpacesDepth = (s) => s.match(/^(\s+)/)?.[0].length || 0
---
+++ tests/javascript/unit/utils/textToTask.spec.js
@@ -0,0 +1,158 @@
+/**
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import {textToTask} from '../../../../src/utils/textToTask.js'
+
+describe('utils/textToTask test suite', () => {
+	it('should convert simple list with empty lines', () => {
+		const text =
+			`    
+- task1
+	
+- task2
+- task3
+`
+
+		const tasks = textToTask(text)
+
+		expect(tasks).toEqual([
+			{title: "task1", children: []},
+			{title: "task2", children: []},
+			{title: "task3", children: []}
+		])
+	})
+
+	it('should convert different prefixes', () => {
+		const text =
+			`
+task
+- task
++ task
+* task
+- [] task
+- [x] task
+- [X] task
+- [ ] task`
+
+		const tasks = textToTask(text)
+
+		const expectedTasks = Array(8).fill({title: "task", children: []})
+		expect(tasks).toEqual(expectedTasks)
+	})
+
+	it('should convert lists with sub-tasks', () => {
+		const text =
+			`
+- task1
+	- task1.1
+		- task1.1.1
+`
+
+		const tasks = textToTask(text)
+
+		const expectedTasks = [
+			{
+				title: "task1", children: [
+					{
+						title: "task1.1", children: [
+							{title: "task1.1.1", children: []},
+						]
+					},
+				]
+			},
+		]
+		expect(tasks).toEqual(expectedTasks)
+	})
+
+	it('should convert lists with uneven indentation', () => {
+		const text =
+			`
+-   task1
+    - task1.1
+     - task1.1.1
+	- task1.2
+ - task1.3
+- task2
+`
+
+		const tasks = textToTask(text)
+
+		const expectedTasks = [
+			{
+				title: "task1", children: [
+					{
+						title: "task1.1", children: [
+							{title: "task1.1.1", children: []},
+						]
+					},
+					{title: "task1.2", children: []},
+					{title: "task1.3", children: []},
+				],
+			},
+			{title: "task2", children: []},
+		]
+		expect(tasks).toEqual(expectedTasks)
+	})
+
+	it('should convert complex lists with sub-tasks', () => {
+		const text =
+			`
+- task1
+	- task1.1
+		- task1.1.1
+		- task1.1.2
+	- task1.2
+		- task1.2.1
+- task2
+- task3
+	- task3.1
+		- task3.1.1
+`
+
+		const tasks = textToTask(text)
+
+		const expectedTasks = [
+			{
+				title: "task1", children: [
+					{
+						title: "task1.1", children: [
+							{title: "task1.1.1", children: []},
+							{title: "task1.1.2", children: []},
+						]
+					},
+					{
+						title: "task1.2", children: [
+							{title: "task1.2.1", children: []},
+						]
+					},
+				]
+			},
+			{title: "task2", children: []},
+			{
+				title: "task3", children: [
+					{
+						title: "task3.1", children: [
+							{title: "task3.1.1", children: []},
+						]
+					},
+				]
+			},
+		]
+		expect(tasks).toEqual(expectedTasks)
+	})
+})
